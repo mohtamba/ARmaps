49d770876c89152ce723fbfcd762baa8d9d7a52e
Nick Holbrook <ndh175@gmail.com>
2021-02-06 16:00:45 -0500

    Setup iOS app

---
+++ ios-app/ARmaps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ARmaps
+//
+//  Created by Nick Holbrook on 2/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
