85e46ab38f8eb726c2aa6127815224698307ab9d
Meixuan Sun <meixuans@umich.edu>
2021-03-04 15:37:26 -0500

    fix bug & half done connecting to backend

--- AR-frontend/ARmap/VenueStore.swift
+++ AR-frontend/ARmap/VenueStore.swift
@@ -1,3 +1,78 @@
 /*
-    TODO: handle venue lists' update
+    TODO
+    1) I guess we don't really need the postVenues function right now?
+            cuz we are not storing any data so far. 
  */
+import Foundation
+struct ChattStore {
+    private let serverUrl = "https://TODO/"
+    func getVenues(refresh: @escaping ([Venue]) -> (),
+                       completion: @escaping () -> ()) {
+            guard let apiUrl = URL(string: serverUrl+"getvenues/") else {
+                print("getVenues: Bad URL")
+                return
+            }
+            
+            var request = URLRequest(url: apiUrl)
+            request.httpMethod = "GET"
+            
+            let task = URLSession.shared.dataTask(with: request) { data, response, error in
+                defer { completion() }
+                guard let data = data, error == nil else {
+                    print("getChatts: NETWORKING ERROR")
+                    return
+                }
+                if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
+                    print("getChatts: HTTP STATUS: \(httpStatus.statusCode)")
+                    return
+                }
+                
+                guard let jsonObj = try? JSONSerialization.jsonObject(with: data) as? [String:Any] else {
+                    print("getChatts: failed JSON deserialization")
+                    return
+                }
+                var venues = [Venue]()
+                let venuesReceived = jsonObj["chatts"] as? [[String?]] ?? []  //TODO: depend on what venues' names are in the tables
+                for venueEntry in venuesReceived {
+                    if (venueEntry.count == Venue.nFields) {
+                        venues += [Venue(venue_name: venueEntry[0],
+                                         distance: venueEntry[1])]
+                    } else {
+                        print("getChatts: Received unexpected number of fields: \(venueEntry.count) instead of \(Venue.nFields).")
+                    }
+                }
+                refresh(venues)
+            }
+            task.resume()
+        }
+//    func postChatt(_ chatt: Venue) {
+//        let jsonObj = ["username": chatt.username,
+//                               "message": chatt.message,
+//                               "audio": chatt.audio]
+//        guard let jsonData = try? JSONSerialization.data(withJSONObject: jsonObj) else {
+//            print("postChatt: jsonData serialization error")
+//            return
+//        }
+//
+//        guard let apiUrl = URL(string: serverUrl+"postchatt/") else {
+//            print("postChatt: Bad URL")
+//            return
+//        }
+//
+//        var request = URLRequest(url: apiUrl)
+//        request.httpMethod = "POST"
+//        request.httpBody = jsonData
+//
+//        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+//            guard let _ = data, error == nil else {
+//                print("postChatt: NETWORKING ERROR")
+//                return
+//            }
+//            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
+//                print("postChatt: HTTP STATUS: \(httpStatus.statusCode)")
+//                return
+//            }
+//        }
+//        task.resume()
+//    }
+}
--- AR-frontend/ARmap/Welcome.swift
+++ AR-frontend/ARmap/Welcome.swift
@@ -4,7 +4,7 @@ import Foundation
     1) add a button so that users can refresh to generate a new list
     2) maybe also show the list of venues in the map
     3) maybe allow users to search nearby venues (add a search window)
-    4) tobe continued
+    4) refresh function to be added in MVP
 */
 
 class Welcome: UITableViewController{
@@ -13,20 +13,22 @@ class Welcome: UITableViewController{
     var try1 = [Venue]()
     var A_to_Z = [String]()
     //add dumb venues--> to be deleted afterwards
-    var miles_example = ["0.3 mile", "0.5 miles", "0.7 miles", "0.3 miles"]
-    var spots_example = ["Harry Potter", "Potter's mum", "Mum's Potter", "Simpsons"]
-    var temp1 = Venue(), temp2 = Venue(), temp3 = Venue(), temp4 = Venue(), temp5 = Venue()
+    var miles_example = ["0.3 mile", "0.5 miles", "0.7 miles", "0.3 miles", "0.7 miles","90 miles"]
+    var spots_example = ["Harry Potter", "Potter's mum", "Mum's Potter", "Simpsons", "Simple","NO"]
+    var temp1 = Venue(), temp2 = Venue(), temp3 = Venue(), temp4 = Venue(), temp5 = Venue(), temp6 = Venue()
     func initialize_things(){
         temp1.distance = miles_example[0]; temp1.venue_name = spots_example[0]
         temp2.distance = miles_example[1]; temp2.venue_name = spots_example[1]
         temp3.distance = miles_example[2]; temp3.venue_name = spots_example[2]
         temp4.distance = miles_example[3]; temp4.venue_name = spots_example[3]
-        //temp5.distance = miles_example[4]; temp5.venue_name = spots_example[4]
-        try1.append(temp1); try1.append(temp2); try1.append(temp3); try1.append(temp4)//; try1.append(temp5)
+        temp5.distance = miles_example[4]; temp5.venue_name = spots_example[4]
+        temp6.distance = miles_example[5]; temp6.venue_name = spots_example[5]
+        try1.append(temp1); try1.append(temp2); try1.append(temp3); try1.append(temp4); try1.append(temp5); try1.append(temp6)
         print("after initialization")
         print(try1)
     }
     override func numberOfSections(in tableView: UITableView) -> Int {
+        //return 1
         return A_to_Z.count
     }
     override func viewDidLoad() {
@@ -50,6 +52,7 @@ class Welcome: UITableViewController{
         
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //return try1.count
         let venue_key = A_to_Z[section]
         if let vee = venue_dictionary[venue_key]{
             return vee.count
@@ -70,9 +73,11 @@ class Welcome: UITableViewController{
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "VenueTableCell", for: indexPath) as? VenueTableCell else {
             fatalError("No reusable cell!")
         }
-        let temp_venues = try1[indexPath.row]
+        let venue_key = A_to_Z[indexPath.section]
+        let vee = venue_dictionary[venue_key]
+        //let temp_venues = try1[indexPath.row]
         print("Problematic!!!")
-        print(temp_venues)
+        let temp_venues = vee![indexPath.row]
         cell.name_venue.text = temp_venues.venue_name
         cell.name_venue.sizeToFit()
         cell.distance.text = temp_venues.distance
