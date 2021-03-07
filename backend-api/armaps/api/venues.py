c1d8155db1dfe54fd643f23154ae3434fc490883
rheidem <rheidem@umich.edu>
2021-03-07 17:08:36 -0500

    finished venues endpoint, serve images

--- backend-api/armaps/api/venues.py
+++ backend-api/armaps/api/venues.py
@@ -12,14 +12,20 @@ import armaps
 
 @armaps.app.route('/api/venues/', methods=["GET"])
 def get_venues():
-    # TODO: complete endpoint
-    connection = armaps.model.get_db()
-    cur = connection.execute('SELECT * FROM venues')
+    # Setup database connection, get all venues
+    cur = armaps.model.get_db()
+    cur.execute("SELECT * FROM venues")
     venues = cur.fetchall()
+
+    # Remove venue_id from each venue dict
+    for venue in venues:
+        venue.pop("venue_id")
+
+    # Sort the venues by name, alphabetically
+    venues = sorted(venues, key=lambda i: i["name"])
 
     context = {
         "data": venues,
         "url": "/api/venues/"
     }
-
     return flask.jsonify(**context)
--- backend-api/armaps/config.py
+++ backend-api/armaps/config.py
@@ -7,6 +7,10 @@ APPLICATION_ROOT = '/'
 # Secret key if we add in cookies
 SECRET_KEY = b'\xb4iu!\xeb\xd4"1U-\xad\xfe.mO]\xde#"N\xc6\x9c\xf8\x9b'
 
+# Where stored images will exist
+ARMAPS_ROOT = pathlib.Path(__file__).resolve().parent.parent
+MEDIA_FOLDER = ARMAPS_ROOT/'uploads'
+
 # Postgres database info
 POSTGRESQL_DATABASE_PORT = 5432
 POSTGRESQL_DATABASE_USER = "armaps_user"
