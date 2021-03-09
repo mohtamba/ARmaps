32dc3edaba4a20c145ce223b46c7c7835a51c45a
mohtamba <59623984+mohtamba@users.noreply.github.com>
2021-03-08 22:26:03 -0500

    Completed barebones destination list

---
+++ AR-frontend/ARmap/Destination.swift
@@ -0,0 +1,8 @@
+
+
+struct Destination{
+    var destination_name: String?
+    var distance: String?
+    //add geodata afterwards
+    static let nFields = 2
+}
--- AR-frontend/ARmap/DestinationVC.swift
+++ AR-frontend/ARmap/DestinationVC.swift
@@ -1,21 +1,20 @@
 import UIKit
 import Foundation
-/*Welcome list todo:
-    1) add a button so that users can refresh to generate a new list
-    2) maybe also show the list of venues in the map
-    3) maybe allow users to search nearby venues (add a search window)
-    4) refresh function to be added in MVP
-*/
 
-class Welcome: UITableViewController{
+
+class DestinationVC: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
+    
+    @IBOutlet weak var venueTitle: UILabel!
+    @IBOutlet weak var destinationList: UITableView!
+    var venueName: String?
     //to create a dictionary of arrays
     var venue_dictionary = [String: [Venue]]()
-    var selectedVenue = Venue()
     var try1 = [Venue]()
     var A_to_Z = [String]()
     //add dumb venues--> to be deleted afterwards
     var miles_example = ["0.3 mile", "0.5 miles", "0.7 miles", "0.3 miles", "0.7 miles","90 miles"]
-    var spots_example = ["Harry Potter", "Potter's mum", "Mum's Potter", "Simpsons", "Simple","NO"]
+    var spots_example = ["Hogwarts", "Olivanders", "Diagon Alley", "Platform 3/4", "Hogwarts Express","Azkeban"]
     var temp1 = Venue(), temp2 = Venue(), temp3 = Venue(), temp4 = Venue(), temp5 = Venue(), temp6 = Venue()
     func initialize_things(){
         temp1.distance = miles_example[0]; temp1.venue_name = spots_example[0]
@@ -28,13 +27,18 @@ class Welcome: UITableViewController{
         print("after initialization")
         print(try1)
     }
-    override func numberOfSections(in tableView: UITableView) -> Int {
+    func numberOfSections(in tableView: UITableView) -> Int {
         //return 1
         return A_to_Z.count
     }
     override func viewDidLoad() {
         initialize_things()
         super.viewDidLoad()
+        self.destinationList.delegate = self
+        self.destinationList.dataSource = self
+        self.destinationList.reloadData()
+        venueTitle.text = venueName
+        venueTitle.sizeToFit()
         //fill in the venue dictionary
         for many in try1{
             let spot_key = String(many.venue_name!.prefix(1))
@@ -52,7 +56,7 @@ class Welcome: UITableViewController{
         //self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "VenueTableCell")
         
     }
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         //return try1.count
         let venue_key = A_to_Z[section]
         if let vee = venue_dictionary[venue_key]{
@@ -60,22 +64,18 @@ class Welcome: UITableViewController{
         }
         return 0;
     }
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
             // event handler when a cell is tapped
-            let venue_key = A_to_Z[indexPath.section]
-            let vee = venue_dictionary[venue_key]
-            selectedVenue = vee![indexPath.row]
             tableView.deselectRow(at: indexPath as IndexPath, animated: true)
-            performSegue(withIdentifier: "showDestinations", sender: self)
         }
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 //        let cell = tableView.dequeueReusableCell(withIdentifier: "VenueTableCell", for: indexPath) as? VenueTableCell
         print("dictionary")
         print(venue_dictionary)
         print("key values")
         print(A_to_Z)
         print(try1)
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "VenueTableCell", for: indexPath) as? VenueTableCell else {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "DestinationTableCell", for: indexPath) as? DestinationTableCell else {
             fatalError("No reusable cell!")
         }
         let venue_key = A_to_Z[indexPath.section]
@@ -83,24 +83,16 @@ class Welcome: UITableViewController{
         //let temp_venues = try1[indexPath.row]
         print("Problematic!!!")
         let temp_venues = vee![indexPath.row]
-        cell.name_venue.text = temp_venues.venue_name
-        cell.name_venue.sizeToFit()
-        cell.distance.text = temp_venues.distance
-        cell.distance.sizeToFit()
+        cell.destination.text = temp_venues.venue_name
+        cell.destination.sizeToFit()
         return cell;
     }
-    override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+    func sectionIndexTitles(for tableView: UITableView) -> [String]? {
         return A_to_Z;
     }
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return A_to_Z[section]
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let dest = segue.destination as? DestinationVC {
-            dest.venueName = selectedVenue.venue_name
-            
-        }
-    }
-    
 }
+
