0e3e37352b5b1fd61e76a8e3f3c0e1ac93351a99
Nick Matton <mattonn@umich.edu>
2021-03-21 13:51:03 -0400

    updated distance code, first attempt at directions

--- backend-api/armaps/api/destinations.py
+++ backend-api/armaps/api/destinations.py
@@ -8,34 +8,13 @@ URLs include:
 
 import flask
 from math import sin, cos, sqrt, atan2, radians
+import geopy.distance
 import armaps
+
 
 def should_order_by_dist(lat, lon):
     """ Returns True if we should order by distance """
-    return abs(lat) <= 180 and abs(lon) <= 180
-
-def get_distance(lat, lon):
-    # approximate radius of earth in km
-    R = 6373.0
-
-    lat1 = radians(lat)
-    lon1 = radians(lon)
-
-    def calc_dist(destination):
-        lat2 = radians(destination['latitude'])
-        lon2 = radians(destination['longitude'])
-
-        dlon = lon2 - lon1
-        dlat = lat2 - lat1
-
-        a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
-        c = 2 * atan2(sqrt(a), sqrt(1 - a))
-        return R * c
-
-    # print("Result:", distance)
-    # print("Should be:", 278.546, "km")
-
-    return calc_dist
+    return abs(lat) <= 90 and abs(lon) <= 180
 
 @armaps.app.route('/api/venues/<int:venueid_url_slug>/destinations/',
                   methods=["GET"])
@@ -52,12 +31,10 @@ def get_destinations(venueid_url_slug):
     )
     destinations = cur.fetchall()
 
-    # Sort by name or by distance
-    # Will not actually sort by distance until
-    # get_distance function is finished
-    get_dist_by_lat_lon = get_distance(lat, lon)
+    user_coords = (lat, lon)
     if should_order_by_dist(lat, lon):
-        destinations = sorted(destinations, key=get_dist_by_lat_lon)
+        destinations = sorted(destinations,
+            key=lambda i: geopy.distance.distance(user_coords, (i['latitude'], i['longitude'])).miles)
     else:
         destinations = sorted(destinations, key=lambda i: i["name"])
 
--- backend-api/armaps/api/venues.py
+++ backend-api/armaps/api/venues.py
@@ -8,17 +8,28 @@ URLs include:
 
 import flask
 import armaps
+from distance import should_order_by_dist
+import geopy.distance
 
 
 @armaps.app.route('/api/venues/', methods=["GET"])
 def get_venues():
+    # Get url parameters if they exist
+    lat = flask.request.args.get("lat", default=999, type=float)
+    lon = flask.request.args.get("lon", default=999, type=float)
+    user_coords = (lat, lon)
+
     # Setup database connection, get all venues
     cur = armaps.model.get_db()
     cur.execute("SELECT * FROM venues")
     venues = cur.fetchall()
 
     # Sort the venues by name, alphabetically
-    venues = sorted(venues, key=lambda i: i["name"])
+    if should_order_by_dist(lat,lon):
+        venues = sorted(venues,
+            key=geopy.distance.distance(user_coords, (i['latitude'], i['longitude'])).miles)
+    else:
+        venues = sorted(venues, key=lambda i: i["name"])
 
     context = {
         "data": venues,
