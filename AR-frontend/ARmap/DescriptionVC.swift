bd7366aef77661a110b048bdcde946e201cc7787
Kate Carlton <katevc@umich.edu>
2021-04-12 01:44:36 -0400

    fixed 0,0 coordinate bug in descriptionVC

--- AR-frontend/ARmap/DescriptionVC.swift
+++ AR-frontend/ARmap/DescriptionVC.swift
@@ -9,61 +9,63 @@ import UIKit
 import SDWebImage
 import CoreLocation
 import Foundation
+import MapKit
 
-class DescriptionVC: UIViewController, CLLocationManagerDelegate{
+class DescriptionVC: UIViewController, CLLocationManagerDelegate {
+    
     var dest: Location?
     var venueId: Int?
     var directions: [[String: Any]]?
     var trydirections = Directions()
     let locationManager = CLLocationManager()
     var destCoordinate = CLLocationCoordinate2D()
-    var refreshControl = UIRefreshControl()
-
+    var userCoord = CLLocationCoordinate2D()
     
     @IBOutlet weak var destinationName: UILabel!
-    
     @IBOutlet weak var destinationDescription: UITextView!
-    
-    
     @IBOutlet weak var destinationImage: UIImageView!
-    
     @IBOutlet weak var destinationDistance: UILabel!
-    
     @IBOutlet weak var destinationTime: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         let imageEnd = String((dest?.imageUrl)!)
         let imageUrl = "https://api.armaps.net\(imageEnd)"
         let placeHolderImage = UIImage(named: "placeholder")!
-        print(imageUrl)
+        /*load image or show placeholder if no image exists - print message if download error*/
         destinationImage .sd_setImage(with: URL(string: imageUrl), placeholderImage: placeHolderImage, options: [.continueInBackground], context: nil, progress: nil, completed: {(downloadedImage, downloadException, cacheType, downloadURL) in
-                    if let downloadException = downloadException {
-                        print("Error downloading image: \(downloadException.localizedDescription)")
-                    } else {
-                        print("successfully downloaded image: \(String(describing: downloadURL?.absoluteString))")
-                    }
-                })
+            if let downloadException = downloadException {
+                print("Error downloading image: \(downloadException.localizedDescription)")
+            } else {
+                print("successfully downloaded image: \(String(describing: downloadURL?.absoluteString))")
+            }
+        })
+        
+        /*assign remaining label values - passed from last scene*/
         destinationName.text = dest?.name
         destinationName.sizeToFit()
         destinationDescription.text = dest?.description
         destinationDescription.sizeToFit()
-        
-        // Do any additional setup after loading the view.
+                
+        // Ask for Authorisation from the User.
+        locationManager.requestAlwaysAuthorization()
 
-        locationManager.delegate = self
+        // For use in foreground
         locationManager.requestWhenInUseAuthorization()
 
-        locationManager.startUpdatingLocation()
-        
-        refresh()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+
+        }
 
     }
-    
-    @objc func handleRefresh(_ refreshControl: UIRefreshControl) {
-        refresh()
-    }
+
+    private func getLabelData() {
         
-    private func refresh() {
+        //getUserCoord()
         
         let store = DirectionStore()
         
@@ -75,33 +77,20 @@ class DescriptionVC: UIViewController, CLLocationManagerDelegate{
             print("missing venue id")
             return
         }
-        // gets authorization status from manager
-        let status = locationManager.authorizationStatus
-        var currentLocation: CLLocation!
 
-        if status == CLAuthorizationStatus.authorizedAlways || status == CLAuthorizationStatus.authorizedWhenInUse {
-            currentLocation = locationManager.location
-                        
-            
-            store.getDirections(venueId: vID, destinationId: destinationId, latitude: Float(currentLocation.coordinate.latitude), longitude: Float(currentLocation.coordinate.longitude), refresh: { directs in
-                self.trydirections = directs
-                print(self.trydirections)
-                DispatchQueue.main.async {
-                    self.destinationTime.text = "Time to Destination: " + String(self.trydirections.time!.intValue) + " minutes"
-                    self.destinationDistance.text = "Distance to Destination: " + String(self.trydirections.distance!.intValue) + " miles"
-                    self.destinationTime.sizeToFit()
-                    self.destinationDistance.sizeToFit()
-                    self.directions = self.trydirections.data
-                }
-            }) {
-                DispatchQueue.main.async {
-                    // stop the refreshing animation upon completion:
-                    self.refreshControl.endRefreshing()
-                }
+        store.getDirections(venueId: vID, destinationId: destinationId, latitude: Float(userCoord.latitude), longitude: Float(userCoord.longitude), refresh: { directs in
+            self.trydirections = directs
+            print(self.trydirections)
+            DispatchQueue.main.async {
+                self.destinationTime.text = "Time to Destination: " + String(self.trydirections.time!.intValue) + " minutes"
+                self.destinationDistance.text = "Distance to Destination: " + String(self.trydirections.distance!.intValue) + " miles"
+                self.destinationTime.sizeToFit()
+                self.destinationDistance.sizeToFit()
+                self.directions = self.trydirections.data
             }
-        }
+        }) {}
     }
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let dest = segue.destination as? ARView2 {
             dest.lat = self.dest?.lat
@@ -115,11 +104,21 @@ class DescriptionVC: UIViewController, CLLocationManagerDelegate{
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
-        self.destCoordinate.latitude = CLLocationDegrees(locValue.latitude)
-        self.destCoordinate.longitude = CLLocationDegrees(locValue.longitude)
+        /* "locations is an array of locations as they change sent from CL */
+        /* we just grab first location we get and stop updating */
+        if let location = locations.first {
+            manager.stopUpdatingLocation()
+            render(location)
+        }
         
     }
     
+    func render(_ location: CLLocation) {
+        userCoord.latitude = location.coordinate.latitude
+        userCoord.longitude = location.coordinate.longitude
+        /*now that we have user loc we can make API call to get label info*/
+        getLabelData()
+
+    }
     
 }
