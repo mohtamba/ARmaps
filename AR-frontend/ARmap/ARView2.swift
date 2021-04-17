a30dce4b0ef2ec74d9dbdb0d7a5c9aec9d1af909
blazecla <44069077+blazecla@users.noreply.github.com>
2021-04-17 12:28:16 -0400

    Waypoints (#36)
    * working on updating waypoints with api call
    
    * waypoints
    
    * errors
    
    * testing api call

--- AR-frontend/ARmap/ARView2.swift
+++ AR-frontend/ARmap/ARView2.swift
@@ -18,9 +18,13 @@ class ARView2: UIViewController {
     var destid: Int?
     var name: String?
     var directions: [[String: Any]]?
+    var new_directions: [[String: Any]]?
     let locationManager = CLLocationManager()
     var destCoordinate = CLLocationCoordinate2D()
     let refreshControl = UIRefreshControl()
+    var waypoint1 = CLLocationCoordinate2D()
+        
+
     var trydirections = Directions()
     
     var updateInfoLabelTimer: Timer?
@@ -31,7 +35,7 @@ class ARView2: UIViewController {
     
     @IBOutlet weak var timeToDest: UILabel!
     @IBOutlet weak var showDestName: UILabel!
-    
+
     //var destNode = LocationAnnotationNode()
     
     /*class func loadFromStoryboard() -> POIViewController {
@@ -43,10 +47,12 @@ class ARView2: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         refreshControl.addTarget(self, action: #selector(DestinationVC.handleRefresh(_:)), for: UIControl.Event.valueChanged)
-
+        print(self.directions)
         locationManager.delegate = self
         locationManager.startUpdatingLocation()
-        
+        //altitude = 60
+        //self.destCoordinate.latitude = CLLocationDegrees(42.427057615617244)
+        //self.destCoordinate.longitude = CLLocationDegrees(-71.41381231790173)
         /*AR setup */
         self.destCoordinate.latitude = CLLocationDegrees(lat!)
         self.destCoordinate.longitude = CLLocationDegrees(lon!)
@@ -67,42 +73,25 @@ class ARView2: UIViewController {
         //*claire testing*//
         
 //        let point1: [String: Any] = [
-//                "lat": 42.427017321626025,
-//                "lon": -71.41558798522215
+//                "lat": 42.427309,
+//                "lon": -71.413880
+//            ]
+       //self.directions![0] = point1
+//        let point2: [String: Any] = [
+//                "lat": 42.427162344731606,
+//                "lon": -71.41387137133471
 //            ]
 //
-//        let point2: [String: Any] = [
-//                "lat": 42.42808642528542,
-//                "lon": -71.41284140334182
+//        let point3: [String: Any] = [
+//                "lat": 42.42711779845314,
+//                "lon": -71.41374933081991
 //            ]
-        
-        
-//        self.directions?.append(point1)
+//
+          //self.directions?.append(point1)
 //        self.directions?.append(point2)
+//        self.directions?.append(point3)
         
-        for point in self.directions ?? []{
-            
-            //var pointCoordinate = CLLocationCoordinate2D()
-            print(point)
-            let lat_val = point["lat"] ?? 0
-            let lon_val = point["lon"] ?? 0
-            
-            let latitude: CLLocationDegrees = lat_val as! CLLocationDegrees
-            let longitude: CLLocationDegrees = lon_val as! CLLocationDegrees
-            
-            var pointCoord = CLLocationCoordinate2D()
-            pointCoord = CLLocationCoordinate2D(latitude: latitude, longitude: longitude);
-            
-            let location2 = CLLocation(coordinate: pointCoord, altitude: Double(altitude!))
-        
-            let image2 = UIImage(named: "pin2")!
-
-            let waypoint = LocationAnnotationNode(location: location2, image: image2)
-            sceneLocationView.addLocationNodeWithConfirmedLocation(locationNode: waypoint)
-        }
-        
-        
-        //getDirectionWaypoints()
+        getDirectionWaypoints()
             
         sceneLocationView.run()
         ContentView.addSubview(sceneLocationView)
@@ -123,6 +112,38 @@ class ARView2: UIViewController {
     }
 
     func monitor_distance(location: CLLocation) {
+        
+        let waypointLoc = CLLocation(coordinate: self.waypoint1, altitude: location.altitude)
+        let waypointdistance = location.distance(from: waypointLoc)
+        
+        //print(waypoint1)
+        
+        if waypointdistance < 10 {
+            print("waypoint reached")
+            //arrivalAlert(message: "Head to next waypoint", entered: true)
+            
+            //sceneLocationView.removeAllNodes()
+            //let new_directions
+            //let notfirst = DropFirstSequence(self.directions!, dropping: 1)
+            //for point in notfirst{
+                //new_directions!.append(point)
+           // }
+            
+            //self.directions = new_directions
+//            let point_1 = self.directions![1]
+//            let lat_val = point_1["lat"] ?? 0
+//            let lon_val = point_1["lon"] ?? 0
+//            let latitude: CLLocationDegrees = lat_val as! CLLocationDegrees
+//            let longitude: CLLocationDegrees = lon_val as! CLLocationDegrees
+//
+//            var pointCoord = CLLocationCoordinate2D()
+//            pointCoord = CLLocationCoordinate2D(latitude: latitude, longitude: longitude);
+//
+//            self.waypoint1 = pointCoord
+            updateWaypoints()
+              //getDirectionWaypoints()
+            
+        }
 
         let destLocation = CLLocation(coordinate: destCoordinate, altitude: location.altitude)
         let distance = location.distance(from: destLocation)
@@ -130,10 +151,133 @@ class ARView2: UIViewController {
         if distance < 10 {
             arrivalAlert(message: "You've reached your destination", entered: true)
             locationManager.stopUpdatingLocation()
+            sceneLocationView.removeAllNodes()
+            
         }
+        
+        
         
     }
     
+
+    func updateWaypoints(){
+        
+        // destroy existing waypoints - sceneLocation.removeAllNodes()
+        print("removenodes")
+        sceneLocationView.removeAllNodes()
+        
+        //pull waypoints
+        print("pull new points")
+        apiDirections()
+        
+        print("display new points")
+        //display new waypoints
+        getDirectionWaypoints()
+        
+        //update distance/time overlay
+    //
+    }
+    
+    func getDirectionWaypoints() {
+        print("getting waypoints")
+        
+        
+        let point_1 = self.directions![0]
+        let lat_val = point_1["lat"] ?? 0
+        let lon_val = point_1["lon"] ?? 0
+        
+        
+    
+        let latitude: CLLocationDegrees = lat_val as! CLLocationDegrees
+        let longitude: CLLocationDegrees = lon_val as! CLLocationDegrees
+    
+        var pointCoord = CLLocationCoordinate2D()
+        pointCoord = CLLocationCoordinate2D(latitude: latitude, longitude: longitude);
+        
+        self.waypoint1 = pointCoord
+    
+        let location2 = CLLocation(coordinate: pointCoord, altitude: Double(altitude!))
+
+        let image2 = UIImage(named: "pin2")!
+
+        let waypoint = LocationAnnotationNode(location: location2, image: image2)
+        sceneLocationView.addLocationNodeWithConfirmedLocation(locationNode: waypoint)
+    
+        let notfirst = DropFirstSequence(self.directions!, dropping: 1)
+        for point in notfirst{
+        
+        //var pointCoordinate = CLLocationCoordinate2D()
+            //print(point)
+            let lat_val = point["lat"] ?? 0
+            let lon_val = point["lon"] ?? 0
+        
+            let latitude: CLLocationDegrees = lat_val as! CLLocationDegrees
+            let longitude: CLLocationDegrees = lon_val as! CLLocationDegrees
+        
+            var pointCoord = CLLocationCoordinate2D()
+            pointCoord = CLLocationCoordinate2D(latitude: latitude, longitude: longitude);
+        
+            let location2 = CLLocation(coordinate: pointCoord, altitude: Double(altitude!))
+    
+            let image2 = UIImage(named: "pinfuturewaypoint")!
+
+            let waypoint = LocationAnnotationNode(location: location2, image: image2)
+            sceneLocationView.addLocationNodeWithConfirmedLocation(locationNode: waypoint)
+        }
+    }
+    
+    func apiDirections(){
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "api.armaps.net"
+        components.path = "/api/venues/" +  String(self.venueid ?? 0) + "/destinations/" + String(self.destid ?? 0) + "/directions/"
+        components.queryItems = [
+            URLQueryItem(name: "lat", value: String(self.destCoordinate.latitude)),
+            URLQueryItem(name: "lon", value: String(self.destCoordinate.longitude))
+        ]
+        guard let apiUrl = components.url else { return  }
+        print(apiUrl)
+        var request = URLRequest(url: apiUrl)
+
+        request.httpMethod = "GET"
+        
+        URLSession.shared.dataTask(with: request) {
+            (data, response, err) in
+            guard let data = data, err == nil else {
+                print("getDestinations: NETWORKING ERROR")
+                return
+            }
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
+                print("getDestinations: HTTP STATUS: \(httpStatus.statusCode)")
+                return
+            }
+            //let jsonObj = try? JSONSerialization.jsonObject(with: data, options: [])
+            guard let jsonObj = try? JSONSerialization.jsonObject(with: data) as? [String:Any] else {
+                print("getDestinations: failed JSON deserialization")
+                return
+            }
+            print(data)
+            var directions = Directions()
+            //let venuesReceived = jsonObj["data"] as ? [String: Any]
+            let directionjson = jsonObj
+            print(directionjson)
+            //TODO: depend on what venues' names are in the tables
+            if (directionjson.count == Directions.nFields + 1) {
+                directions = Directions(data:(directionjson["data"] as! [[String:Any]]),
+                                        distance: (directionjson["distance"] as! NSNumber),
+                                        time: (directionjson["time_estimate"] as! NSNumber))
+            } else {
+                print("getDest: Received unexpected number of fields: \(String(describing: directionjson.count)).")
+            }
+            DispatchQueue.main.async {
+                self.directions = directions.data
+
+                
+            }
+
+        }.resume()
+    }
+
     @objc
     func updateInfoLabel() {
         //timeToDest.text = " Hello this is a test \n"
@@ -184,6 +328,7 @@ class ARView2: UIViewController {
 //            sceneLocationView.addLocationNodeWithConfirmedLocation(locationNode: annotationNode)
 //        }
 //    }
+
 }
 
 // MARK: Helper Extensions
