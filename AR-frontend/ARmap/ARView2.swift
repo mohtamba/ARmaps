00c4946ca422ebbab6b182b15d6ffe80b00eb337
Nick Holbrook <ndh175@gmail.com>
2021-04-23 09:15:48 -0400

    Final updates

--- AR-frontend/ARmap/ARView2.swift
+++ AR-frontend/ARmap/ARView2.swift
@@ -36,14 +36,6 @@ class ARView2: UIViewController {
     
     @IBOutlet weak var timeToDest: UILabel!
     @IBOutlet weak var showDestName: UILabel!
-
-    //var destNode = LocationAnnotationNode()
-    
-    /*class func loadFromStoryboard() -> POIViewController {
-        return UIStoryboard(name: "Main", bundle: nil)
-            .instantiateViewController(withIdentifier: "ARView2") as! POIViewController
-        // swiftlint:disable:previous force_cast
-    }*/
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,13 +43,10 @@ class ARView2: UIViewController {
         print(self.directions)
         locationManager.delegate = self
         locationManager.startUpdatingLocation()
-        //altitude = 60
-        //self.destCoordinate.latitude = CLLocationDegrees(42.427057615617244)
-        //self.destCoordinate.longitude = CLLocationDegrees(-71.41381231790173)
+
         /*AR setup */
         self.destCoordinate.latitude = CLLocationDegrees(lat!)
         self.destCoordinate.longitude = CLLocationDegrees(lon!)
-        //let destCoordinate = CLLocationCoordinate2D(latitude: CLLocationDegrees(lat!), longitude: CLLocationDegrees(lon!))
         let location = CLLocation(coordinate: destCoordinate, altitude: Double(altitude!))
         
         let image = UIImage(named: "pin")!
@@ -70,27 +59,6 @@ class ARView2: UIViewController {
         updateInfoLabelTimer = Timer.scheduledTimer(withTimeInterval: 15, repeats: true) { [weak self] _ in
             self?.updateInfoLabel()
         }
-        
-        //*claire testing*//
-        
-//        let point1: [String: Any] = [
-//                "lat": 42.427309,
-//                "lon": -71.413880
-//            ]
-       //self.directions![0] = point1
-//        let point2: [String: Any] = [
-//                "lat": 42.427162344731606,
-//                "lon": -71.41387137133471
-//            ]
-//
-//        let point3: [String: Any] = [
-//                "lat": 42.42711779845314,
-//                "lon": -71.41374933081991
-//            ]
-//
-          //self.directions?.append(point1)
-//        self.directions?.append(point2)
-//        self.directions?.append(point3)
         
         getDirectionWaypoints()
             
@@ -114,37 +82,14 @@ class ARView2: UIViewController {
 
     func monitor_distance(location: CLLocation) {
         
-        let waypointLoc = CLLocation(coordinate: self.waypoint1, altitude: location.altitude)
-        let waypointdistance = location.distance(from: waypointLoc)
-        
-        //print(waypoint1)
-        
-        if waypointdistance < 10 {
-            print("waypoint reached")
-            //arrivalAlert(message: "Head to next waypoint", entered: true)
-            
-            //sceneLocationView.removeAllNodes()
-            //let new_directions
-            //let notfirst = DropFirstSequence(self.directions!, dropping: 1)
-            //for point in notfirst{
-                //new_directions!.append(point)
-           // }
-            
-            //self.directions = new_directions
-//            let point_1 = self.directions![1]
-//            let lat_val = point_1["lat"] ?? 0
-//            let lon_val = point_1["lon"] ?? 0
-//            let latitude: CLLocationDegrees = lat_val as! CLLocationDegrees
-//            let longitude: CLLocationDegrees = lon_val as! CLLocationDegrees
+//        let waypointLoc = CLLocation(coordinate: self.waypoint1, altitude: location.altitude)
+//        let waypointdistance = location.distance(from: waypointLoc)
 //
-//            var pointCoord = CLLocationCoordinate2D()
-//            pointCoord = CLLocationCoordinate2D(latitude: latitude, longitude: longitude);
+//        if waypointdistance < 10 {
+//            print("waypoint reached")
 //
-//            self.waypoint1 = pointCoord
-            updateWaypoints()
-              //getDirectionWaypoints()
-            
-        }
+//            updateWaypoints()
+//        }
 
         let destLocation = CLLocation(coordinate: destCoordinate, altitude: location.altitude)
         let distance = location.distance(from: destLocation)
@@ -153,7 +98,6 @@ class ARView2: UIViewController {
             arrivalAlert(message: "You've reached your destination", entered: true)
             locationManager.stopUpdatingLocation()
             sceneLocationView.removeAllNodes()
-            
         }
         
         
@@ -171,8 +115,8 @@ class ARView2: UIViewController {
         print("pull new points")
         apiDirections()
         
-        print("display new points")
         //display new waypoints
+        print("display new points")
         getDirectionWaypoints()
         
         //update distance/time overlay
@@ -187,8 +131,6 @@ class ARView2: UIViewController {
         let lat_val = point_1["lat"] ?? 0
         let lon_val = point_1["lon"] ?? 0
         let bearing = point_1["bearing"] ?? 0
-        
-        
     
         let latitude: CLLocationDegrees = lat_val as! CLLocationDegrees
         let longitude: CLLocationDegrees = lon_val as! CLLocationDegrees
@@ -220,7 +162,7 @@ class ARView2: UIViewController {
         
             let location2 = CLLocation(coordinate: pointCoord, altitude: Double(altitude!))
             
-            let unrotated = UIImage(named: "arrow2")!
+            let unrotated = UIImage(named: "arrow1")!
             let image2 = unrotated.rotate(radians: bearing as! CGFloat)
 
             let waypoint = LocationAnnotationNode(location: location2, image: image2)
@@ -299,7 +241,7 @@ class ARView2: UIViewController {
                 self.trydirections = directs
                 print(self.trydirections)
                 DispatchQueue.main.async {
-                    self.timeToDest.text = "Time to Destination: " + String(self.trydirections.time!.intValue + 1) + " minutes"
+                    self.timeToDest.text = "Time to Waypoint: " + String(self.trydirections.time!.intValue) + " minutes"
                 }
             }) {
                
